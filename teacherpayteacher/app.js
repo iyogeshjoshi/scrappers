9bc03c8f4bd5b63ca00deb5654474319c12f489d
iyogeshjoshi <iyogeshjoshi@gmail.com>
2015-05-22 14:09:32 +0530

    added directory for new scrapper

---
+++ teacherpayteacher/app.js
@@ -0,0 +1,20 @@
+/**
+ * Scrapper for teacher pay teacher
+ * @author Yogesh Joshi<iyogeshjoshi@gmail.com>
+ */
+
+var fs = require('fs');
+var stringify = require('csv-stringify');
+var async = require('async');
+
+
+// Number of pages
+var COUNT = 1;
+var LIMIT = 1000;
+
+var URL = 'https://www.teacherspayteachers.com/Browse/Seller/Order:Best-Seller-All-Time/Page:';
+
+// create URLs
+async.series([
+
+]);
